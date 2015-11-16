3face338b467e8299965d940aa131a8c3c657259
Matt Ellis <m.t.ellis@gmail.com>
2015-11-16 09:57:58 +0000

    Updated to ReSharper 10

--- src/CyclomaticComplexity/Properties/AssemblyInfo.cs
+++ src/CyclomaticComplexity/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly : AssemblyVersion("3.0.0.0")]
-[assembly : AssemblyFileVersion("3.0.0.0")]
+[assembly : AssemblyVersion("10.0.0.0")]
+[assembly : AssemblyFileVersion("10.0.0.0")]
