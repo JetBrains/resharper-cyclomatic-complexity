dbe4a22f25eda869389710c69dbd37ef66dfd87c
Matthias Koch <ithrowexceptions@gmail.com>
2017-11-07 15:13:38 +0100

    Add nuke build.

---
+++ build/Build.cs
@@ -0,0 +1,85 @@
+﻿// /*
+//  * Copyright 2007-2015 JetBrains
+//  *
+//  * Licensed under the Apache License, Version 2.0 (the "License");
+//  * you may not use this file except in compliance with the License.
+//  * You may obtain a copy of the License at
+//  *
+//  * http://www.apache.org/licenses/LICENSE-2.0
+//  *
+//  * Unless required by applicable law or agreed to in writing, software
+//  * distributed under the License is distributed on an "AS IS" BASIS,
+//  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  * See the License for the specific language governing permissions and
+//  * limitations under the License.
+//  */
+
+using System;
+using System.Linq;
+using System.Web.UI.WebControls.WebParts;
+using Nuke.Common.Tools;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Tools.NuGet;
+using Nuke.Common.Tools.Nunit;
+using Nuke.Core;
+using Nuke.Core.IO;
+using Nuke.Core.Tooling;
+using Nuke.Core.Utilities.Collections;
+using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
+using static Nuke.Common.Tools.NuGet.NuGetTasks;
+using static Nuke.Common.Tools.Nunit.NunitTasks;
+using static Nuke.Core.IO.FileSystemTasks;
+using static Nuke.Core.IO.PathConstruction;
+
+class Build : NukeBuild
+{
+  public static int Main () => Execute<Build>(x => x.Compile);
+
+  [GitVersion] readonly GitVersion GitVersion;
+  [Parameter] readonly string MyGetApiKey;
+
+  Target Clean => _ => _
+      .OnlyWhen(() => false) // Disabled for safety.
+      .Executes(() =>
+      {
+        DeleteDirectories(GlobDirectories(SourceDirectory, "**/bin", "**/obj"));
+        EnsureCleanDirectory(OutputDirectory);
+      });
+
+  Target Restore => _ => _
+      .DependsOn(Clean)
+      .Executes(() =>
+      {
+        MSBuild(s => DefaultMSBuildRestore);
+      });
+
+  Target Compile => _ => _
+      .DependsOn(Restore)
+      .Executes(() =>
+      {
+        MSBuild(s => DefaultMSBuildCompile);
+      });
+
+  Target Test => _ => _
+      .DependsOn(Compile)
+      .Executes(() =>
+      {
+        Nunit3(s => s
+            .AddInputFiles(GlobFiles(RootDirectory / "test", $"**/bin/{Configuration}/tests.dll"))
+            .EnableNoResults()
+            .SetToolPath(ToolPathResolver.GetPackageExecutable("NUnit.ConsoleRunner", "nunit3-console.exe")));
+      });
+
+  Target Pack => _ => _
+      .DependsOn(Compile)
+      .Executes(() =>
+      {
+        GlobFiles(RootDirectory / "install", "*.nuspec")
+            .ForEach(x => NuGetPack(s => DefaultNuGetPack
+                .SetTargetPath(x)
+                .SetBasePath(RootDirectory / "install")
+                .SetOutputDirectory(OutputDirectory)));
+      });
+
+
+}
