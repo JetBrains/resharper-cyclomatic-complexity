09cd617e79a7612c52663d33f750444ea666ba3b
Matthias Koch <ithrowexceptions@gmail.com>
2020-11-25 07:43:44 +0100

    Update SDK and build

--- settings.ps1
+++ settings.ps1
@@ -1,4 +1,3 @@
-$PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 $PluginId = "ReSharperPlugin.CyclomaticComplexity"
 $SolutionPath = "$PSScriptRoot\CyclomaticComplexity.sln"
 $SourceBasePath = "$PSScriptRoot\src\dotnet"
